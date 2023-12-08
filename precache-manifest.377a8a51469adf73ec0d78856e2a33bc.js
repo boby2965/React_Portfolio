28fa56708785fa08369167da4a34f012e9aac3e9
Chaitanya <boby2965@gmail.com>
2023-12-09 00:12:42 +0530

    Updates

---
+++ precache-manifest.377a8a51469adf73ec0d78856e2a33bc.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36329e62d41a3393f7c4c6e27be5ec61",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "23a2f2bf939dd7a054ac",
+    "url": "/React_Portfolio/static/css/main.9a9456c3.chunk.css"
+  },
+  {
+    "revision": "92af080f9eee5aef2e2b",
+    "url": "/React_Portfolio/static/js/2.f9d3aae0.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/React_Portfolio/static/js/2.f9d3aae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a2f2bf939dd7a054ac",
+    "url": "/React_Portfolio/static/js/main.1f4b1ee0.chunk.js"
+  },
+  {
+    "revision": "42edcba433767c2f4b9f",
+    "url": "/React_Portfolio/static/js/runtime-main.9f4c8ff0.js"
+  },
+  {
+    "revision": "47ccce3e6e080fe3ffb1ae3e410ff43b",
+    "url": "/React_Portfolio/static/media/ChaitanyaLogo2.47ccce3e.png"
+  },
+  {
+    "revision": "821bcd2bd781e04937c60c5bbdbbaf17",
+    "url": "/React_Portfolio/static/media/about01.821bcd2b.png"
+  },
+  {
+    "revision": "0b4e9372915752a0d9db5fc50c416263",
+    "url": "/React_Portfolio/static/media/about02.0b4e9372.png"
+  },
+  {
+    "revision": "7c0e05806ce3588a62fc7f42383c2e48",
+    "url": "/React_Portfolio/static/media/about03.7c0e0580.png"
+  },
+  {
+    "revision": "6d81249e0a1ce75f945e8b53a7702400",
+    "url": "/React_Portfolio/static/media/about04.6d81249e.png"
+  },
+  {
+    "revision": "79f17773cfd09c7c6a04da0998ad7521",
+    "url": "/React_Portfolio/static/media/adidas.79f17773.png"
+  },
+  {
+    "revision": "b6dc087f26802781d7b69f624dc68b1c",
+    "url": "/React_Portfolio/static/media/amazon.b6dc087f.png"
+  },
+  {
+    "revision": "cacd18992c794c7cad00e0f4c7510aa6",
+    "url": "/React_Portfolio/static/media/asus.cacd1899.png"
+  },
+  {
+    "revision": "a6b819e7d95b900cde1602932e3260d9",
+    "url": "/React_Portfolio/static/media/bgIMG.a6b819e7.png"
+  },
+  {
+    "revision": "df093b364e58bd1a14d2f1cfec122e76",
+    "url": "/React_Portfolio/static/media/bgWhite.df093b36.png"
+  },
+  {
+    "revision": "d4eab336c0d9c813acc384dad328e240",
+    "url": "/React_Portfolio/static/media/bolt.d4eab336.png"
+  },
+  {
+    "revision": "9300f69fe407f85e9fcd74215502f8b5",
+    "url": "/React_Portfolio/static/media/circle.9300f69f.svg"
+  },
+  {
+    "revision": "8949bfc86fc10ef1505994eb643e940b",
+    "url": "/React_Portfolio/static/media/cpp.8949bfc8.png"
+  },
+  {
+    "revision": "5c4e16ceb77211d7b286e351e272328d",
+    "url": "/React_Portfolio/static/media/css.5c4e16ce.png"
+  },
+  {
+    "revision": "a02ffeea0446038face9dd264d702d47",
+    "url": "/React_Portfolio/static/media/email.a02ffeea.png"
+  },
+  {
+    "revision": "338e06da5dc18e11f4b2199aef456cf3",
+    "url": "/React_Portfolio/static/media/figma.338e06da.png"
+  },
+  {
+    "revision": "9a073b4cb55995549f7dc759eb46f831",
+    "url": "/React_Portfolio/static/media/git.9a073b4c.png"
+  },
+  {
+    "revision": "1bcf5ba870770831c6b0963c2b796bcb",
+    "url": "/React_Portfolio/static/media/graphql.1bcf5ba8.png"
+  },
+  {
+    "revision": "714e15000e8da0b1ababfa6fc6f9dd01",
+    "url": "/React_Portfolio/static/media/kid_comp.714e1500.jpg"
+  },
+  {
+    "revision": "07a9dce226e6e930853ab55ed1dce0c3",
+    "url": "/React_Portfolio/static/media/mobile.07a9dce2.png"
+  },
+  {
+    "revision": "df61ee2c437e25eb2124525664552dc5",
+    "url": "/React_Portfolio/static/media/nb.df61ee2c.png"
+  },
+  {
+    "revision": "19370d004b6acb679a64e08c4dcf3ba0",
+    "url": "/React_Portfolio/static/media/node.19370d00.png"
+  },
+  {
+    "revision": "c5c90730815f6ae6915097d5bed4134e",
+    "url": "/React_Portfolio/static/media/profile.c5c90730.png"
+  },
+  {
+    "revision": "c62e7fbf2959f445c0695c7aa460a2a0",
+    "url": "/React_Portfolio/static/media/redux.c62e7fbf.png"
+  },
+  {
+    "revision": "c88a1a314fa1bc4bcd50b79e21b168a1",
+    "url": "/React_Portfolio/static/media/sass.c88a1a31.png"
+  },
+  {
+    "revision": "e99b220a2081b5bc624a4240aab1e3b8",
+    "url": "/React_Portfolio/static/media/skype.e99b220a.png"
+  },
+  {
+    "revision": "9cc17d1bf96c54082bcf8e8208c001f2",
+    "url": "/React_Portfolio/static/media/spotify.9cc17d1b.png"
+  }
+]);
